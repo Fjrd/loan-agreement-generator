7f063a237384c96b3e390b56a192aaa0c206d664
Reztsov Alexander <areztsov@neoflex.ru>
2021-11-15 14:39:59 +0300

    init

---
+++ src/main/java/com/example/loanagreementgenerator/LoanAgreementGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.loanagreementgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanAgreementGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanAgreementGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loanagreementgenerator/LoanAgreementGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanagreementgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanAgreementGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
